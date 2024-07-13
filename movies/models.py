339b22576d6249c87c437e294286f3330323c046
impesantez <168923512+impesantez@users.noreply.github.com>
2024-07-13 13:49:50 -0500

    Add files via upload

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model): 
+    movie_title = models.CharField(max_length=50, null=False)
+    movie_genre = models.CharField(max_length=30, null=False)
+    movie_director = models.CharField(max_length=60, null=False)
+    release_year = models.IntegerField(null=False, default=2000)
+    synopsis = models.CharField(max_length=200, null=False) 
+    
+    def __str__(self) -> str:
+        return self.movie_title 
+    
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Movie
 
-# Create your views here.
+def index(request):
+    movies = movies.objects.order_by('type')
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'movie': movie}, request))
+
+def movie(request, movie_id):
+    movie = movie.objects.get(id=movie_id)
+    template = loader.get_template('display_movie.html')
+    context = {
+        'movie': movie
+    }
+    return HttpResponse(template.render(context, request))
